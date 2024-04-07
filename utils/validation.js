ecde8a31e27e4ccbb6c3b33561434ab4c20866fd
pbgnz <pbgnz@users.noreply.github.com>
2024-04-07 00:25:36 -0400

    fix  #4 inefficient regular expression

--- utils/validation.js
+++ utils/validation.js
@@ -2,6 +2,6 @@ exports.validateUrl = function (url) {
     if (url.length > 200) {
         throw new Error("URL exceeds the maximum length of 200 characters");
     }
-    const regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+    const regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w.-]+)?\/?$/;
     return regex.test(url);    
 };
