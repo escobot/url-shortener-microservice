8880a414d393d5919df9dcd5b3972df478de29b1
pbgnz <pabgonza1995@gmail.com>
2024-04-09 20:32:55 -0400

    #5 add rate limiting

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const rateLimit = require('express-rate-limit');
+
+const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false
+});
 
 const { generateShortUrl, getLongUrl } = require('./services/url.service');
 
 app.use(express.json());
 
-app.get('/new/:url', generateShortUrl);
-app.get('/get/:url', getLongUrl);
+app.get('/new/:url', rateLimiter, generateShortUrl);
+app.get('/get/:url', rateLimiter, getLongUrl);
 
 module.exports = app;
